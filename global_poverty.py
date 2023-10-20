62a1051c9d940200a9f1bc24ec10dd0bc0b6073a
aloenne-qery <alf.lonne@qery.no>
2023-10-20 18:40:45 +0200

    added two sub-regions to drop

--- global_poverty.py
+++ global_poverty.py
@@ -21,13 +21,13 @@ df_new.to_csv('data/Extreme_Poverty_World_Population.csv', index=True)
 #Extreme Poverty Share Region
 df_csv = pd.read_csv('https://api.worldbank.org/pip/v1/pip-grp?country=all&year=all&group_by=wb&format=csv')
 df_new = df_csv.pivot(index='reporting_year', columns='region_name', values='headcount')*100
-df_new.drop(['World'], axis=1,inplace=True)
+df_new.drop(['World', 'Eastern and Southern Africa', 'Western and Central Africa'], axis=1, inplace=True)
 df_new.to_csv('data/Extreme_Poverty_Regions_Share.csv', index=True)
 
 #Extreme Poverty Population Region
 df_csv = pd.read_csv('https://api.worldbank.org/pip/v1/pip-grp?country=all&year=all&group_by=wb&format=csv')
 df_new = df_csv.pivot(index='reporting_year', columns='region_name', values='pop_in_poverty')
-df_new.drop(['World'], axis=1,inplace=True)
+df_new.drop(['World', 'Eastern and Southern Africa', 'Western and Central Africa'], axis=1, inplace=True)
 df_new.to_csv('data/Extreme_Poverty_Regions_Population.csv', index=True)
 
 #Extreme Poverty Country Profile
